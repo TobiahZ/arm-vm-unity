37e0a9149a649d03832df7fe39fbce0a57661e0c
Justin Wendlandt <juswen@microsoft.com>
2020-02-11 18:09:04 -0800

    Updating powershell

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -26,13 +26,13 @@ Configuration Main
     
             SetScript = {
                 Install-Module UnitySetup
-                Find-UnitySetupInstaller -Version '$using:UnityVersion'  -Components $using:UnityComponents | Install-UnitySetupInstance
+                Find-UnitySetupInstaller -Version "$using:UnityVersion"  -Components $using:UnityComponents | Install-UnitySetupInstance
     
                 Add-Type -AssemblyName System.IO.Compression.FileSystem
                 $WebClient = New-Object System.Net.WebClient
                 $WebClient.DownloadFile("https://vstsagentpackage.azureedge.net/agent/2.164.7/vsts-agent-win-x64-2.164.7.zip", "c:\agent\vsts-agent.zip")
                 [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\agent\vsts-agent.zip", "c:\agent\vsts-agent")
-                Start-Process -WorkingDirectory "c:\agent\vsts-agent" -ArgumentList "--unattended --url $using:AzdoUrl --auth pat --token $using:AzdoToken --runAsService" .\config.cmd
+                Start-Process -WorkingDirectory "c:\agent\vsts-agent" -ArgumentList --unattended --url "$using:AzdoUrl" --auth pat --token "$using:AzdoToken" --runAsService .\config.cmd
             }
         }
     }
