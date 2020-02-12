d48412ea0ed6aefa31306b5ef9644a76519c5706
Justin Wendlandt <juswen@microsoft.com>
2020-02-11 16:13:01 -0800

    Updating powershell script.

--- InstallPackages.ps1
+++ InstallPackages.ps1
@@ -8,25 +8,27 @@ Configuration Main
     )
 
     Node 'localhost' {
-		GetScript = {
-            @{
-                Result = ""
+        Script InstallRequirements {
+            GetScript = {
+                @{
+                    Result = ""
+                }
             }
-        }
-
-        TestScript = {
-            $false
-        }
-
-        SetScript = {
-            Install-Module UnitySetup -AllowPrerelease
-            Find-UnitySetupInstaller -Version '$UnityVersion'  -Components $UnityComponents | Install-UnitySetupInstance
-
-            Add-Type -AssemblyName System.IO.Compression.FileSystem
-            $WebClient = New-Object System.Net.WebClient
-            $WebClient.DownloadFile("https://vstsagentpackage.azureedge.net/agent/2.164.7/vsts-agent-win-x64-2.164.7.zip", "c:\agent\vsts-agent.zip")
-            [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\agent\vsts-agent.zip", "c:\agent\vsts-agent")
-            Start-Process -WorkingDirectory "c:\agent\vsts-agent" -ArgumentList "--unattended --url $AzdoUrl --auth pat --token $AzdoToken --runAsService" .\config.cmd
+    
+            TestScript = {
+                $false
+            }
+    
+            SetScript = {
+                Install-Module UnitySetup -AllowPrerelease
+                Find-UnitySetupInstaller -Version '$UnityVersion'  -Components $UnityComponents | Install-UnitySetupInstance
+    
+                Add-Type -AssemblyName System.IO.Compression.FileSystem
+                $WebClient = New-Object System.Net.WebClient
+                $WebClient.DownloadFile("https://vstsagentpackage.azureedge.net/agent/2.164.7/vsts-agent-win-x64-2.164.7.zip", "c:\agent\vsts-agent.zip")
+                [System.IO.Compression.ZipFile]::ExtractToDirectory("c:\agent\vsts-agent.zip", "c:\agent\vsts-agent")
+                Start-Process -WorkingDirectory "c:\agent\vsts-agent" -ArgumentList "--unattended --url $AzdoUrl --auth pat --token $AzdoToken --runAsService" .\config.cmd
+            }
         }
     }
 }
